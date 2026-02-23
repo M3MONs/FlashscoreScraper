0a26e1a80d256f2099acfe63a7388d92e1794674
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-02-23 18:47:02 +0100

    refactor: update registration of FootballOdds to use Sport enum for better clarity

--- odds/football_odds.py
+++ odds/football_odds.py
@@ -1,7 +1,8 @@
 from odds.odds_factory import BaseOdds, register_odds
+from utils.detect_sport import Sport
 
 
-@register_odds("football")
+@register_odds(Sport.FOOTBALL.value)
 class FootballOdds(BaseOdds):
     ONE_X_TWO = "1x2-odds"
     OVER_UNDER = "over-under"
