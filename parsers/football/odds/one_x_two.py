2f9e5c48500dc7df85014bd5bf51c03deb58323b
M3MONs <alan.skrrrz@gmail.com>
2026-03-03 18:44:32 +0100

    feat: implement parsing logic in FootballOneXTwoParser class

--- parsers/football/odds/one_x_two.py
+++ parsers/football/odds/one_x_two.py
@@ -1,4 +1,6 @@
 from typing import Any, Dict
+
+from bs4 import BeautifulSoup
 from odds.football_odds import FootballOdds
 from parsers.base_odds_parser import BaseOddsParser
 from utils.detect_sport import Sport
@@ -9,8 +11,28 @@ class FootballOneXTwoParser(BaseOddsParser):
     odds_type = FootballOdds.ONE_X_TWO.value
 
     def parse(self, url: str, data: Any) -> Dict[str, Any]:
-        return {
-            "odds_type": "1x2",
-            "data": "parsed_1x2_data",
-            "error": None
-        }
+        soup = BeautifulSoup(data, "html.parser")
+
+        wrapper = soup.find("div", class_="oddsTab__tableWrapper")
+
+        if not wrapper:
+            return {"odds_type": "1x2", "data": None, "error": "wrapper_not_found"}
+
+        results = []
+
+        rows = wrapper.select(".ui-table__row")
+
+        for row in rows:
+            img_tag = row.select_one(".oddsCell__bookmakerPart img")
+            bookmaker = img_tag.get("alt") if img_tag else "Unknown"
+            link_tag = row.select_one('.wcl-bookmakerLogo_4IUU0 a')
+            bookmaker_link = link_tag.get('href') if link_tag else None
+
+            odds_spans = row.select("a.oddsCell__odd span")
+
+            odds_values = [span.get_text(strip=True) for span in odds_spans]
+
+            if len(odds_values) >= 3:
+                results.append({"bookmaker": bookmaker, "bookmaker_link": bookmaker_link, "1": odds_values[0], "X": odds_values[1], "2": odds_values[2]})
+
+        return {"odds_type": "1x2", "data": results, "error": None}
