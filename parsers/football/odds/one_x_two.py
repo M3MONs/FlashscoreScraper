cd0c74abcda54b87717150f89b8098ad875d7a93
M3MONs <alan.skrrrz@gmail.com>
2026-03-16 21:43:37 +0100

    refactor: remove unused static methods in FootballOneXTwoParser

--- parsers/football/odds/one_x_two.py
+++ parsers/football/odds/one_x_two.py
@@ -6,6 +6,7 @@ from odds.football_odds import FootballOdds
 from parsers.base_odds_parser import BaseOddsParser
 from utils.detect_sport import Sport
 
+
 @dataclass
 class FootballOneXTwoParserResult:
     bookmaker: str
@@ -14,7 +15,9 @@ class FootballOneXTwoParserResult:
     odds_X: str
     odds_2: str
 
+
 MIN_ODDS_COUNT = 3
+
 
 class FootballOneXTwoParser(BaseOddsParser):
     sport_type = Sport.FOOTBALL.value
@@ -32,11 +35,11 @@ class FootballOneXTwoParser(BaseOddsParser):
         results = [r for r in results if r is not None]
 
         return results
-    
+
     def _parse_row(self, row: Tag) -> FootballOneXTwoParserResult | None:
         try:
             odds_values = self._extract_odds(row)
-            
+
             if len(odds_values) < MIN_ODDS_COUNT:
                 return None
 
@@ -50,19 +53,7 @@ class FootballOneXTwoParser(BaseOddsParser):
         except Exception as e:
             self.logger.error(f"Error parsing row: {e}")
             return None
-    
+
     @staticmethod
     def _extract_odds(row: Tag) -> list[str]:
         return [span.get_text(strip=True) for span in row.select("a.oddsCell__odd span")]
-    
-    @staticmethod
-    def _extract_bookmaker_name(row: Tag) -> str:
-        img_tag = row.select_one(".oddsCell__bookmakerPart img")
-        return str(img_tag.get("alt")) if img_tag else "Unknown"
-    
-    @staticmethod
-    def _extract_bookmaker_link(row: Tag) -> str | None:
-        link_tag = row.select_one('.wcl-bookmakerLogo_4IUU0 a')
-        return str(link_tag.get('href')) if link_tag else None
-
-    
