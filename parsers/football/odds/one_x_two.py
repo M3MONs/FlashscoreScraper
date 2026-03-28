c95e21dc2585f3f092cd11e3b311c388c729a5b9
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-03-28 12:19:19 +0100

    refactor: remove FootballOneXTwoParserResult class and update _parse_row to return OddsParserRow

--- parsers/football/odds/one_x_two.py
+++ parsers/football/odds/one_x_two.py
@@ -1,17 +1,8 @@
-from dataclasses import dataclass
 from bs4 import Tag
 from models.odds.football_odds import FootballOdds
+from models.odds_parser_row import OddsParserRow, OddsParserRowData
 from parsers.base_odds_parser import BaseOddsParser, BookmakerInfo
 from utils.detect_sport import Sports
-
-
-@dataclass
-class FootballOneXTwoParserResult:
-    bookmaker: str
-    bookmaker_link: str | None
-    odds_1: str
-    odds_X: str
-    odds_2: str
 
 
 class FootballOneXTwoParser(BaseOddsParser):
@@ -19,16 +10,16 @@ class FootballOneXTwoParser(BaseOddsParser):
     odds_type = FootballOdds.ONE_X_TWO.value
     ODDS_COUNT = 3
 
-    def _parse_row(self, row: Tag, bookmaker: BookmakerInfo) -> FootballOneXTwoParserResult | None:
+    def _parse_row(self, row: Tag, bookmaker: BookmakerInfo) -> OddsParserRow | None:
         odds_values = self._extract_odds(row)
 
         if len(odds_values) is not self.ODDS_COUNT:
             return None
 
-        return FootballOneXTwoParserResult(
-            bookmaker=bookmaker.name,
-            bookmaker_link=bookmaker.link,
-            odds_1=odds_values[0],
-            odds_X=odds_values[1],
-            odds_2=odds_values[2],
-        )
+        odds_values = [
+            OddsParserRowData(value="1", type="1", odds=odds_values[0]),
+            OddsParserRowData(value="X", type="X", odds=odds_values[1]),
+            OddsParserRowData(value="2", type="2", odds=odds_values[2]),
+        ]
+
+        return self._generate_result(bookmaker, odds_values)
