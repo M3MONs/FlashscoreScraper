d10d1e9f76af86cec56a2c5812b976fc6d4852d4
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-03-28 12:19:23 +0100

    refactor: remove OverUnderParserResult class and update _parse_row to return OddsParserRow

--- parsers/football/odds/over-under.py
+++ parsers/football/odds/over-under.py
@@ -1,17 +1,8 @@
-from dataclasses import dataclass
 from bs4 import Tag
 from models.odds.football_odds import FootballOdds
+from models.odds_parser_row import OddsParserRow, OddsParserRowData
 from parsers.base_odds_parser import BaseOddsParser, BookmakerInfo
 from utils.detect_sport import Sports
-
-
-@dataclass
-class OverUnderParserResult:
-    bookmaker: str
-    bookmaker_link: str | None
-    total: str
-    odds_over: str
-    odds_under: str
 
 
 class FootballOverUnderParser(BaseOddsParser):
@@ -19,18 +10,16 @@ class FootballOverUnderParser(BaseOddsParser):
     odds_type = FootballOdds.OVER_UNDER.value
     ODDS_COUNT = 2
 
-    def _parse_row(self, row: Tag, bookmaker: BookmakerInfo) -> OverUnderParserResult | None:
+    def _parse_row(self, row: Tag, bookmaker: BookmakerInfo) -> OddsParserRow | None:
         total = self._extract_value(row)
         odds_values = self._extract_odds(row)
 
         if len(odds_values) is not self.ODDS_COUNT:
             return None
 
-        return OverUnderParserResult(
-            bookmaker=bookmaker.name,
-            bookmaker_link=bookmaker.link,
-            total=total,
-            odds_over=odds_values[0],
-            odds_under=odds_values[1],
-        )
+        odds_values = [
+            OddsParserRowData(value=total, type="over", odds=odds_values[0]),
+            OddsParserRowData(value=total, type="under", odds=odds_values[1]),
+        ]
 
+        return self._generate_result(bookmaker, odds_values)
