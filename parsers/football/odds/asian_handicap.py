cf50eb4cdb42a661733c3ca5eabd920747e1ae01
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-03-28 12:19:13 +0100

    refactor: remove FootballAsianHandicapParserResult class and update _parse_row to return OddsParserRow

--- parsers/football/odds/asian_handicap.py
+++ parsers/football/odds/asian_handicap.py
@@ -1,17 +1,8 @@
-from dataclasses import dataclass
+from models.odds_parser_row import OddsParserRow, OddsParserRowData
 from parsers.base_odds_parser import BaseOddsParser, BookmakerInfo
 from bs4 import Tag
 from utils.detect_sport import Sports
 from models.odds.football_odds import FootballOdds
-
-
-@dataclass
-class FootballAsianHandicapParserResult:
-    bookmaker: str
-    bookmaker_link: str | None
-    handicap: str
-    odds_1: str
-    odds_2: str
 
 
 class AsianHandicapParser(BaseOddsParser):
@@ -19,16 +10,15 @@ class AsianHandicapParser(BaseOddsParser):
     odds_type = FootballOdds.ASIAN_HANDICAP.value
     ODDS_COUNT = 2
 
-    def _parse_row(self, row: Tag, bookmaker: BookmakerInfo) -> FootballAsianHandicapParserResult | None:
+    def _parse_row(self, row: Tag, bookmaker: BookmakerInfo) -> OddsParserRow | None:
         odds_values = self._extract_odds(row)
 
         if len(odds_values) is not self.ODDS_COUNT:
             return None
 
-        return FootballAsianHandicapParserResult(
-            bookmaker=bookmaker.name,
-            bookmaker_link=bookmaker.link,
-            handicap=self._extract_value(row),
-            odds_1=odds_values[0],
-            odds_2=odds_values[1],
-        )
+        odds_values = [
+            OddsParserRowData(value=self._extract_value(row), type="handicap", odds=odds_values[0]),
+            OddsParserRowData(value=self._extract_value(row), type="handicap", odds=odds_values[1]),
+        ]
+
+        return self._generate_result(bookmaker, odds_values)
