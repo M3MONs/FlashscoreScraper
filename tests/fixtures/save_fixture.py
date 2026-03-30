275f7de542c4a67bda450c6bb3f3c50f805eeea8
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-03-30 19:18:14 +0200

    refactor: use Path for file path construction in save_fixture script

--- tests/fixtures/save_fixture.py
+++ tests/fixtures/save_fixture.py
@@ -1,9 +1,12 @@
 import logging
+from pathlib import Path
 from engines.engine_factory import create_engine
 from models.sports import Sports
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+FILE_PATH = Path(__file__).parent
 
 
 URLS = [
@@ -15,7 +18,7 @@ engine = create_engine("playwright", timeout=15)
 try:
     for url_info in URLS:
         html = engine.get_page(url_info["url"])
-        file_path = f"tests/fixtures/{url_info['sport'].name.lower()}_event.html"
+        file_path = FILE_PATH / f"{url_info['sport'].name.lower()}_event.html"
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(html)
         logger.info(f"{url_info['sport'].name} match page saved successfully.")
