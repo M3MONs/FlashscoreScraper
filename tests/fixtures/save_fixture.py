661c85f06ee0bcb0c9ff1a3a9511155a2f9a2fe5
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-03-30 18:54:40 +0200

    feat: refactor save_fixture to handle multiple URLs and improve logging

--- tests/fixtures/save_fixture.py
+++ tests/fixtures/save_fixture.py
@@ -1,17 +1,23 @@
 import logging
 from engines.engine_factory import create_engine
+from models.sports import Sports
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-URL = "https://www.flashscore.com/match/football/aston-villa-W00wmLO0/west-ham-Cxq57r8g/?mid=U7CL8Og5"
+
+URLS = [
+    {"url": "https://www.flashscore.com/match/football/aston-villa-W00wmLO0/west-ham-Cxq57r8g/?mid=U7CL8Og5", "sport": Sports.FOOTBALL},
+]
 
 engine = create_engine("playwright", timeout=15)
 
 try:
-    html = engine.get_page(URL)
-    with open("tests/fixtures/football_match.html", "w", encoding="utf-8") as f:
-        f.write(html)
-    logger.info("Football match page saved successfully.")
+    for url_info in URLS:
+        html = engine.get_page(url_info["url"])
+        file_path = f"tests/fixtures/{url_info['sport'].name.lower()}_event.html"
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(html)
+        logger.info(f"{url_info['sport'].name} match page saved successfully.")
 finally:
     engine.close()
