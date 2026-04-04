bb849627e8fbfbde96c385858584be7ac03a200b
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-04-04 15:03:22 +0200

    feat: add method to build event info URL in FlashscoreUrlBuilder

--- utils/url_builder.py
+++ utils/url_builder.py
@@ -9,3 +9,11 @@ class FlashscoreUrlBuilder:
         new_path = posixpath.join(parsed.path, "odds", odds_key)
         parsed = parsed._replace(path=new_path, query="", fragment="")
         return urlunparse(parsed)
+
+    @staticmethod
+    def build_event_info_url(event_url: str, url_path: str) -> str:
+        parsed = urlparse(event_url)
+        base_path = parsed.path.rstrip("/") + "/"
+        new_path = posixpath.join(base_path, url_path)
+        parsed = parsed._replace(path=new_path, query="", fragment="")
+        return urlunparse(parsed)
