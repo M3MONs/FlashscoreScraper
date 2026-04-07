653854ef78896caac9eb1653dcdf24c4bc39e92c
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-04-07 18:06:07 +0200

    feat: update FootballEventInfo DRAW URL and add draw-related data classes

--- models/event_info/football_event_info.py
+++ models/event_info/football_event_info.py
@@ -8,7 +8,7 @@ from utils.detect_sport import Sports
 @register_event_info(Sports.FOOTBALL.value)
 class FootballEventInfo(BaseEventInfo):
     STANDINGS = ("standings", "standings/standings/overall/")
-    DRAW = ("draw", "draw/draw/")
+    DRAW = ("draw", "draw/")
 
     @property
     def tab_label(self) -> str:
@@ -17,6 +17,13 @@ class FootballEventInfo(BaseEventInfo):
     @property
     def url_path(self) -> str:
         return self.value[1]
+
+    @property
+    def wait_for_selector(self) -> str | None:
+        _selectors = {
+            FootballEventInfo.DRAW: "div.draw",
+        }
+        return _selectors.get(self)
 
 
 @dataclass
@@ -41,3 +48,25 @@ class FootballStandingsEntry:
     points: str | None
     form: list[FootballFormEntry] = field(default_factory=list)
     highlighted: bool = False
+
+
+@dataclass
+class FootballDrawParticipant:
+    name: str | None
+    is_home: bool
+    is_advancing: bool
+    score: str | None
+
+
+@dataclass
+class FootballDrawMatch:
+    home: FootballDrawParticipant | None
+    away: FootballDrawParticipant | None
+    is_eventless: bool
+    is_highlighted: bool
+
+
+@dataclass
+class FootballDrawRound:
+    round_name: str | None
+    matches: list[FootballDrawMatch] = field(default_factory=list)
