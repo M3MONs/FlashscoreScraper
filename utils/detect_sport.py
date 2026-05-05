9b39750c278fbeab39a43efcc884c4c98877bd7e
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-05-05 19:43:45 +0200

    feat: enhance sport detection logic to support multiple sports from enum

--- utils/detect_sport.py
+++ utils/detect_sport.py
@@ -4,7 +4,7 @@ from models.sports import Sports
 # TODO: Improve sport detection logic to handle more sports and edge cases
 def detect_sport_from_url(url: str) -> str:
     """Detects the sport type from the given URL."""
-    if Sports.FOOTBALL.value in url.lower():
-        return Sports.FOOTBALL.value
-    else:
-        raise ValueError(f"Cannot detect sport type from URL: {url}")
+    for sport in Sports:
+        if sport.value in url.lower():
+            return sport.value
+    raise ValueError(f"Cannot detect sport type from URL: {url}")
